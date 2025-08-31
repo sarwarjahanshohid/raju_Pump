78d3282b9501641448e8ae0b568befa704267006
sarwarjahanshohid <sarwarjahanshohid@gmail.com>
2025-09-01 00:09:15 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
